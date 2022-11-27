d6bd92024eec18dd1cf0ce5013a3387dc99de7d5
trinhqtkd1994 <trinhqtkd1994@gmail.com>
2022-11-27 18:45:45 +0700

    firt init

---
+++ app/src/main/java/com/lasadu/videos_sharing/splash/ui/SplashScreenActivity.kt
@@ -0,0 +1,14 @@
+package com.lasadu.videos_sharing.splash.ui
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lasadu.videos_sharing.R
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
